b98d1f0a60e7bf4352d3149ddff90a22c3fe06da
Sung Yoo <yoowook1207@gmail.com>
2022-04-15 14:04:31 -0400

    single home page updated

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
     return (
         <section>
-            <h1 id="about">Who am I?</h1>
+            <h1>Who am I?</h1>
             <div className="my-2">
                 <p>
                 Full stack web developer with video graphical knowledge to build more fascinating user experience on the web. Recently graduated from the George Washington University Coding Bootcamp, with newly learned skills in JavaScript, Css, React.js and responsive web design. Studied Film and Video studies from George Mason University and able to use Adobe Premiere Pro, After Effects, Photoshop and Illustrator. Having confidence not only in front-end skills but also in back-end skills. Understanding how user’s data flows in the program, and looking for the efficient way to optimize the web service environments. I am always concentrating on problem solving. Imagine diverse solutions in my head and find out the most efficient way with enough discussion with my teammates. I am excited to use my skills in the real web development fields.
